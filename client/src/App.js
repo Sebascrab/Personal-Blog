9789fb187f587b15f7ae7f75a14bafc8d9cc9418
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-02 13:31:35 -0700

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Single from './pages/Single';
 import Home from './pages/Home';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import "./style.scss";
 
 const Layout = () => {
   return (
