6a6c9233fcf8afbf3287b61f47123d9f13b24dba
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-03 12:19:21 -0700

    home page logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
 
 const Home = () => {
+
+    const posts = [
+        {
+            id: 1, 
+            title: "Cool Programing Stuff", 
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            img:"https://c.pxhere.com/photos/4b/a8/work_typing_computer_notebook_programming_business_technology_internet-883504.jpg!d" 
+        },
+        {
+            id: 2, 
+            title: "Learning How to Code? ", 
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            img: "https://c.pxhere.com/photos/74/63/business_cellphone_codes_coding_computer_data_desk_display-1366348.jpg!d" 
+        },
+        {
+            id: 3, 
+            title: "Tips and Tricks for C#", 
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            img: "https://c.pxhere.com/photos/a9/1c/beverage_browsing_business_coffee_computer_connection_cup_desk-1522939.jpg!d"
+        },
+        {
+            id: 4, 
+            title: "Working with Third-Pary-Api's", 
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            img: "https://c.pxhere.com/photos/83/8e/camera_photography_lens_equipment_photographer_technology_digital_photographic-1074009.jpg!d" 
+        },
+    ]
     return (
         <div className='home'>
-            
+            <div className='posts'>
+                {posts.map((post) => (
+                    <div className='post' key={post.id}>
+                        <div className="img">
+                            <img src={post.img} alt="" />
+                        </div>
+                        <div className="content">
+                            <Link to={`/post/${post.id}`}>
+                            <h1>{post.title}</h1>
+                            <p>{post.desc}</p>
+                            <button>Read More</button>
+                            </Link>
+                        </div>
+                    </div>
+                    ))}
+            </div>
         </div>
     )
 }
