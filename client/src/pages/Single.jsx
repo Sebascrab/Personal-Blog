f37a2e0beff23906ad04db70ab0f4db5f9d3ab51
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-05 20:33:22 -0700

    changes to write page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -9,9 +9,9 @@ const Single = () => {
     return (
         <div className='single'>
             <div className="content">
-                <img src="https://c.pxhere.com/photos/83/8e/camera_photography_lens_equipment_photographer_technology_digital_photographic-1074009.jpg!d" alt="post image" />
+                <img src="https://c.pxhere.com/photos/83/8e/camera_photography_lens_equipment_photographer_technology_digital_photographic-1074009.jpg!d" alt="post" />
                 <div className="user">
-                    <img src="https://c.pxhere.com/photos/a1/8c/airplane_aircraft_avi_o_buero_aeroclube_aeroclubedepernambuco_puaft_aerobuero-422249.jpg!d" alt="profile picture" />
+                    <img src="https://c.pxhere.com/photos/a1/8c/airplane_aircraft_avi_o_buero_aeroclube_aeroclubedepernambuco_puaft_aerobuero-422249.jpg!d" alt="profile" />
                     <div className="info">
                         <span>John</span>
                         <p>Posted 2 days ago</p>
