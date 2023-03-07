991ae1426d23f30c18db8d5913fd2de58f49b61b
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-07 10:54:37 -0700

    register page linked to db

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 const Register = () => {
 
@@ -9,12 +10,22 @@ const Register = () => {
         password: "", 
     });
 
+    const [err, setError] = useState(null);
+    const navigate = useNavigate();
+
     const handleChange = e => {
         setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
     }
     
     const handleSubmit = async e => {
         e.preventDefault()
+        try {
+            
+            await axios.post("/auth/register", inputs);
+           navigate('/login');
+        } catch(err) {
+           setError(err.response.data)
+        }
     }
 
     return (
@@ -25,7 +36,7 @@ const Register = () => {
                 <input required type="email" placeholder='email' name='email' onChange={handleChange} />
                 <input required type="password" placeholder='password' name='password' onChange={handleChange} />
                 <button onClick={handleSubmit}>Register</button>
-                <p>This is an error!</p>
+                {err && <p>{err}</p>}
                 <span>Do you have an account? <Link to="/login">Login</Link></span>
             </form>
         </div>
