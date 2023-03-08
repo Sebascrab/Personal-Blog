31da72d19dae9b0f879ca82d7f83f9ab62483ddb
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-07 23:26:52 -0700

    adding routes and starting functions for user post blogs

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,22 @@
+
+
+
+export const getPosts = (req, res) => {
+    const q = 
+}
+
+export const getPost = (req, res) => {
+    res.json('from controller')
+}
+
 export const addPost = (req, res) => {
+    res.json('from controller')
+}
+
+export const deletePost = (req, res) => {
+    res.json('from controller')
+}
+
+export const updatePost = (req, res) => {
     res.json('from controller')
 }
