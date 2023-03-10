bf991c8ad0db1f61000e58f149daa7bf6fb762e1
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-09 22:02:51 -0700

    fixing bugs on single post

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,21 +1,29 @@
-import {db} from '../db.js';
+import { db } from '../db.js';
 
 // all posts    
 export const getPosts = (req, res) => {
-   const q = req.query.cat 
-   ? "SELECT * FROM posts WHERE cat=?" 
-   : "SELECT * FROM posts";
+    const q = req.query.cat
+        ? "SELECT * FROM posts WHERE cat=?"
+        : "SELECT * FROM posts";
 
-   db.query(q, [req.query.cat], (err, data) => {
-       if (err) {
-           return res.json(err)
-       } return res.status(200).json(data);
-   })
+    db.query(q, [req.query.cat], (err, data) => {
+        if (err) {
+            return res.json(err)
+        } return res.status(200).json(data);
+    })
 }
+
+
 
 // single post
 export const getPost = (req, res) => {
-    res.json('from controller')
+    const q = "SELECT `username`, `title`, `desc`, `p.img`, `u.img AS userImg, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ? "
+
+    db.query(q,[req.params.id], (err, data) => {
+        if(err) return res.json(err)
+
+        return res.status(200).json(data[0])
+    })
 }
 
 export const addPost = (req, res) => {
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,37 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom'
 import Menu from '../components/Menu'
 
 import Edit from '../images/Edit-Icon.png';
 import Delete from '../images/Delete-Icon-3.jpeg';
+import axios from 'axios';
+import moment from 'moment';
+import { AuthContext } from '../context/authContext';
 
 const Single = () => {
+
+    const [post, setPost] = useState({});
+
+    const location = useLocation()
+    
+    const postId = location.pathname.split('/')[2]
+
+    const {currentUser} = useContext(AuthContext)
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const res = await axios.get(`/posts/${postId}`)
+                setPost(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, [postId]);
+
     return (
         <div className='single'>
             <div className="content">
-                <img src="https://c.pxhere.com/photos/83/8e/camera_photography_lens_equipment_photographer_technology_digital_photographic-1074009.jpg!d" alt="post" />
+                <img src={post?.img} alt="post" />
                 <div className="user">
-                    <img src="https://c.pxhere.com/photos/a1/8c/airplane_aircraft_avi_o_buero_aeroclube_aeroclubedepernambuco_puaft_aerobuero-422249.jpg!d" alt="profile" />
+                    <img src={post.userImg} alt="profile" />
                     <div className="info">
-                        <span>John</span>
-                        <p>Posted 2 days ago</p>
+                        <span>{post.username}</span>
+                        <p>Posted {moment(post.date).fromNow()}</p>
                     </div>
+                    {currentUser.username === post.username && (
                     <div className="edit">
                         <Link to={`/write?edit=2`}>
                         <img src={Edit} alt="edit button" />
                         </Link>
                         <img src={Delete} alt="delete button" />
                     </div>
+                     )}
                 </div>
-                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-                    <br />
-                    <br />
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-                    </p>
-                </p>
+                <h1>{post.title}</h1>
+                    <p>{post.desc}</p>
             </div>
             <Menu />
         </div>
