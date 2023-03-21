f0876d72d1cc41b2f92241f7934d68b71f0284b2
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-21 09:13:53 -0600

    added function to return posts for user

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
+
+import axios from 'axios';
+import React, {useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const User = () => {
 
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get('/posts')
+                setPosts(res.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData();
+    }, [])
+
+
+
+    const getText = (html) => {
+        const doc = new DOMParser().parseFromString(html, "text/html")
+        return doc.body.textContent
+    }
+
     return (
         <div className='user-page'>
+            <div className='user'>
+                {posts.map((post) => (
+                    <div className='post' key={post.id}>
+                        <div className='img'>
+                            <img src={`../upload/${post.img}`} alt=""/>
+                        </div>
+                        <div className='content'>
+                            <Link className='link' to={`/post/${post.id}`}>
+                                <h1>{getText(post.title)}</h1>
+                            </Link>
+                            <p>{getText(post.desc)}</p>
+                            <Link className='link' to={`/post/${post.id}`}>
+                                <button>ReadMore</button>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
             <div className='user-info'>
-Hello world
+
             </div>
 
         </div>
