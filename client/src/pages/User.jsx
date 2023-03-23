99dab6aea92ed626ad09d233eebb6d3ab967309f
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-23 00:56:01 -0600

    added user info

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,7 +1,8 @@
 
 import axios from 'axios';
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import profilePic from '../images/Sebas-dog.jpeg'
 
 const User = () => {
 
@@ -19,6 +20,10 @@ const User = () => {
         fetchData();
     }, [])
 
+    
+
+
+
 
 
     const getText = (html) => {
@@ -26,13 +31,15 @@ const User = () => {
         return doc.body.textContent
     }
 
+
+
     return (
         <div className='user-page'>
             <div className='user-posts'>
                 {posts.map((post) => (
                     <div className='user-post' key={post.id}>
                         <div className='post-img'>
-                            <img src={`../upload/${post.img}`} alt=""/>
+                            <img src={`../upload/${post.img}`} alt="" />
                         </div>
                         <div className='post-content'>
                             <Link className='link' to={`/post/${post.id}`}>
@@ -40,18 +47,27 @@ const User = () => {
                             </Link>
                             <p>{getText(post.desc)}</p>
                             <Link className='link' to={`/post/${post.id}`}>
-                                <button>ReadMore</button>
+                                <button value="userEdit">ReadMore</button>
                             </Link>
                         </div>
                     </div>
                 ))}
             </div>
             <div className='user-info'>
-
+                <div className='profile-container'>
+                    <img className='profile-img' src={profilePic} />
+                    <div className="profile-info">
+                        <h3>Sebastain Tischner</h3>
+                        <h5>Username: Sebascrab</h5>
+                        <h5>alexanders.tischner@outlook.com</h5>
+                        <button>Edit</button>
+                    </div>
+                </div>
             </div>
 
         </div>
     )
+                
 }
 
 export default User
