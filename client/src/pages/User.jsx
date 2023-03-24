bb39efe883a66db391de87d88bfc3749046cbf7e
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-23 23:48:45 -0600

    added function for displaying user info on user page

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation,  } from 'react-router-dom';
 import profilePic from '../images/Sebas-dog.jpeg'
 
 const User = () => {
@@ -13,6 +13,23 @@ const User = () => {
             try {
                 const res = await axios.get('/posts')
                 setPosts(res.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData();
+    }, [])
+
+    const location = useLocation();
+    const userId = location.pathname;
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(`/users/${userId}`)
+                setUsers(res.data)
             } catch (err) {
                 console.log(err)
             }
@@ -56,14 +73,17 @@ const User = () => {
             <div className='user-info'>
                 <div className='profile-container'>
                     <img className='profile-img' src={profilePic} />
+                        {users.map((user) => (
                     <div className="profile-info">
-                        <h3>Sebastain Tischner</h3>
-                        <h5>Username: Sebascrab</h5>
-                        <h5>alexanders.tischner@outlook.com</h5>
+                            
+                        <h3>{user.name}</h3>
+                        <h5>Username: {user.username}</h5>
+                        <h5>{user.email}</h5>
                         <Link to='/edit'>
                             <button>Edit</button>
                         </Link>
                     </div>
+                        ))}
                 </div>
             </div>
 
