42747f3c54415a4ba31cf297f51c61e77e5ef420
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-04-03 16:17:21 -0600

    changes

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@
 import mysql from 'mysql2';
 
 export const db = mysql.createConnection({
-    host: 'localhost', 
+    host: '127.0.0.1', 
     user: 'root', 
     password: '3577', 
     database: 'blog',
